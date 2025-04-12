de371d9a967f98e98d66958791d5ea19ab7aa624
uniker9 <uniker9@gmail.com>
2025-04-12 12:07:20 +0900

    add auto-kdk data

---
+++ qmk_firmware/keyboards/split_us40/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0x13, 0x90, 0x69, 0x94, 0xb3, 0xb4, 0x77, 0xdc}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
